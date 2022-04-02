c14ee98eddb1421de8ef529dcafbb1ede4b15892
Agam4095 <100255294+Agam4095@users.noreply.github.com>
2022-04-02 14:10:29 +0530

    update 2

--- src/main/java/Cab/Service/demo/model/TripBooking.java
+++ src/main/java/Cab/Service/demo/model/TripBooking.java
@@ -3,18 +3,15 @@ package Cab.Service.demo.model;
 
 import java.time.LocalDateTime;
 
+
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity
 @Table(name="TripBooking_table")

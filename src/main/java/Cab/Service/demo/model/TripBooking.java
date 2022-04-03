a97b93d618cbb5c9e0c0dedc843b2a9825334a44
Sajal Ganjewala <sg748@snu.edu.in>
2022-04-03 16:44:23 +0530

    Admin Semi

---
+++ src/main/java/Cab/Service/demo/model/TripBooking.java
@@ -0,0 +1,110 @@
+package Cab.Service.demo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+//enum TripStatus {
+//	  IN_PROGRESS,
+//	  FINISHED
+//	}
+
+@Entity
+@Table(name = "TripBooking_table")
+//@Data
+public class TripBooking {
+	@Id
+	@GeneratedValue
+	private int tripBookingId;
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+	@JoinColumn(name = "customer_Id")
+	private Customer customer;
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+	@JoinColumn(name = "driver_Id")
+	private Driver driver;
+	private String fromLocation;
+	private String toLocation;
+	private LocalDateTime fromDateTime;
+	private LocalDateTime toDateTime;
+	private boolean status;
+	private float distanceInKm;
+	private float bill;
+	public int getTripBookingId() {
+		return tripBookingId;
+	}
+	public void setTripBookingId(int tripBookingId) {
+		this.tripBookingId = tripBookingId;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Driver getDriver() {
+		return driver;
+	}
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+	public String getFromLocation() {
+		return fromLocation;
+	}
+	public void setFromLocation(String fromLocation) {
+		this.fromLocation = fromLocation;
+	}
+	public String getToLocation() {
+		return toLocation;
+	}
+	public void setToLocation(String toLocation) {
+		this.toLocation = toLocation;
+	}
+	public LocalDateTime getFromDateTime() {
+		return fromDateTime;
+	}
+	public void setFromDateTime(LocalDateTime fromDateTime) {
+		this.fromDateTime = fromDateTime;
+	}
+	public LocalDateTime getToDateTime() {
+		return toDateTime;
+	}
+	public void setToDateTime(LocalDateTime toDateTime) {
+		this.toDateTime = toDateTime;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public float getDistanceInKm() {
+		return distanceInKm;
+	}
+	public void setDistanceInKm(float distanceInKm) {
+		this.distanceInKm = distanceInKm;
+	}
+	public float getBill() {
+		return bill;
+	}
+	public void setBill(float bill) {
+		this.bill = bill;
+	}
+	@Override
+	public String toString() {
+		return "TripBooking [tripBookingId=" + tripBookingId + ", customer=" + customer + ", driver=" + driver
+				+ ", fromLocation=" + fromLocation + ", toLocation=" + toLocation + ", fromDateTime=" + fromDateTime
+				+ ", toDateTime=" + toDateTime + ", status=" + status + ", distanceInKm=" + distanceInKm + ", bill="
+				+ bill + "]";
+	}
+	
+	
+	
+	
+
+}

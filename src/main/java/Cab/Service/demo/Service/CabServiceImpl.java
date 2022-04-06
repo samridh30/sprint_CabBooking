fc5f53caa490238a2707e658eeffb16736aae824
samridh30 <97982455+samridh30@users.noreply.github.com>
2022-04-07 01:54:30 +0530

    update 2

--- src/main/java/Cab/Service/demo/Service/CabServiceImpl.java
+++ src/main/java/Cab/Service/demo/Service/CabServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import Cab.Service.demo.Exception.CabNotFoundException;
+import Cab.Service.demo.Exception.InvalidUserException;
 import Cab.Service.demo.Exception.UserNotLoggedInException;
 import Cab.Service.demo.model.Cab;
 import Cab.Service.demo.model.Role;
@@ -22,8 +23,9 @@ public class CabServiceImpl implements ICabService {
 
 	@Override
 	public Cab insertCab(Cab cab) {
-		if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
-			return car_repo.save(cab);
+		if (AppUser.loggedInUser.toString() != null) {
+			if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
+				return car_repo.save(cab);
 //			Optional<Cab> car = car_repo.findById(cab.getCabId());
 //			if (car.isEmpty()) {
 //				throw new CabNotFoundException("Cab not found");
@@ -31,36 +33,49 @@ public class CabServiceImpl implements ICabService {
 //				car_repo.save(cab);
 //				return cab;
 //			}
+			} else {
+				throw new InvalidUserException("Not logged in as admin");
+			}
 		} else {
 			throw new UserNotLoggedInException("Login First");
-
 		}
 	}
+	
 
 	@Override
 	public Cab updateCab(Cab cab) {
-		if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
-			Optional<Cab> car = car_repo.findById(cab.getCabId());
+		if (AppUser.loggedInUser.toString() != null) {
+			if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
+				Optional<Cab> car = car_repo.findById(cab.getCabId());
 
-			if (car.isPresent()) {
-				car_repo.save(cab);
-				return cab;
+				if (car.isPresent()) {
+					car_repo.save(cab);
+					return cab;
+				} else {
+					throw new CabNotFoundException("Invalid Cab");
+				}
 			} else {
-				throw new CabNotFoundException("Invalid Customer");
+				throw new InvalidUserException("Not logged in as admin");
 			}
+		} else {
+			throw new UserNotLoggedInException("Login First");
 		}
-		throw new UserNotLoggedInException("Login First");
 	}
+
 
 	@Override
 	public Cab deleteCab(int cabId) {
-		if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
-			Optional<Cab> car = car_repo.findById(cabId);
-			if (car.isPresent()) {
-				car_repo.deleteById(cabId);
-				return car.get();
+		if (AppUser.loggedInUser.toString() != null) {
+			if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
+				Optional<Cab> car = car_repo.findById(cabId);
+				if (car.isPresent()) {
+					car_repo.deleteById(cabId);
+					return car.get();
+				} else {
+					throw new CabNotFoundException("Invalid Id");
+				}
 			} else {
-				throw new CabNotFoundException("Invalid Id");
+				throw new InvalidUserException("Not logged in as admin");
 			}
 		} else {
 			throw new UserNotLoggedInException("Login First");
@@ -69,9 +84,13 @@ public class CabServiceImpl implements ICabService {
 
 	@Override
 	public List<Cab> viewCabsOfType(String carType) {
-		if (AppUser.loggedInUser.getRole() == Role.CUSTOMER) {
-			List<Cab> car = car_repo.findByCarType(carType);
-			return car;
+		if (AppUser.loggedInUser.toString() != null) {
+			if (AppUser.loggedInUser.getRole() == Role.CUSTOMER) {
+				List<Cab> car = car_repo.findByCarType(carType);
+				return car;
+			} else {
+				throw new InvalidUserException("not logged in as admin");
+			}
 		} else {
 			throw new UserNotLoggedInException("Login First");
 		}
@@ -80,9 +99,13 @@ public class CabServiceImpl implements ICabService {
 
 	@Override
 	public int countCabsOfType(String carType) {
-		if (AppUser.loggedInUser.getRole() == Role.CUSTOMER) {
-			int car = car_repo.countByCarType(carType);
-			return car;
+		if (AppUser.loggedInUser.toString() != null) {
+			if (AppUser.loggedInUser.getRole() == Role.CUSTOMER) {
+				int car = car_repo.countByCarType(carType);
+				return car;
+			} else {
+				throw new InvalidUserException("Not logged in as Admin");
+			}
 		} else {
 			throw new UserNotLoggedInException("Login First");
 		}
@@ -90,11 +113,19 @@ public class CabServiceImpl implements ICabService {
 
 	@Override
 	public List<Cab> viewAllCabs() {
-		if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
-			return car_repo.findAll();
+		if (AppUser.loggedInUser.toString() != null) {
+			if (AppUser.loggedInUser.getRole() == Role.ADMIN) {
+				return car_repo.findAll();
+			} else {
+				throw new InvalidUserException("Not logged in as admin");
+			}
+		} else {
+			throw new UserNotLoggedInException("Login first");
 		}
-		return null;
 
 	}
-
 }
+
+	
+
+

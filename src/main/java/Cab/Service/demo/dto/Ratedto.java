1ddecac3d652b8c4baa4ff03118801fc3d64240c
Agam4095 <100255294+Agam4095@users.noreply.github.com>
2022-05-01 19:53:13 +0530

    rate

--- src/main/java/Cab/Service/demo/dto/Ratedto.java
+++ src/main/java/Cab/Service/demo/dto/Ratedto.java
@@ -2,47 +2,55 @@ package Cab.Service.demo.dto;
 
 import org.springframework.stereotype.Component;
 
-
-import Cab.Service.demo.model.Cab;
 @Component
-public class Driverdto {
+public class Ratedto {
+
 	int driverId;
 	float rating;
-	Cab cab;
 	
 	
-	public Driverdto() {
+	public Ratedto() {
 		super();
 	}
-	public Driverdto(int driverId, float rating, Cab cab) {
+
+
+	public Ratedto(int driverId, float rating) {
 		super();
 		this.driverId = driverId;
 		this.rating = rating;
-		this.cab = cab;
 	}
+
+
 	public int getDriverId() {
 		return driverId;
 	}
+
+
 	public void setDriverId(int driverId) {
 		this.driverId = driverId;
 	}
+
+
 	public float getRating() {
 		return rating;
 	}
+
+
 	public void setRating(float rating) {
 		this.rating = rating;
 	}
-	public Cab getCab() {
-		return cab;
-	}
-	public void setCab(Cab cab) {
-		this.cab = cab;
-	}
+
+
 	@Override
 	public String toString() {
-		return "Driverdto [driverId=" + driverId + ", rating=" + rating + ", cab=" + cab + "]";
+		return "Rundto [driverId=" + driverId + ", rating=" + rating + "]";
 	}
 	
 	
-
+	
+	
+	
+	
 }
+
+
